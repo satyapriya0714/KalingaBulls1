c72a0d602d847ae99075ca565c7ac208e5e30037
satyapriya swain <satyapriya0714@gmail.com>
2024-02-02 00:04:56 +0530

    Add project files.

---
+++ KalingaBulls/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KalingaBulls
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ KalingaBulls/Models/Home.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KalingaBulls.Models
+{
+    public class Home
+    {
+        public int HomeId { get; set; }
+    }
+}
